4738e8638268f1059d20caa92851cb64cc669abe
westelh <reo.retriever@gmail.com>
2017-03-18 16:21:57 +0900

    Brush up finalizer module

--- include/singleton/singleton_finalizer.h
+++ include/singleton/singleton_finalizer.h
@@ -6,15 +6,12 @@
 
 namespace elh{
     class singleton_finalizer final {
-            using finalize_func = void (*)();
+            static std::array<std::function<void()>, MAX_FINALIZERS_SIZE> finalizers;
         public:
             static void add_finalizer(std::function<void()> func);
             static int finalizers_size() noexcept;
             static void finalize() noexcept;
             static constexpr int MAX_FINALIZERS_SIZE = 50;
-
-        private:
-            static std::array<std::function<void()>, MAX_FINALIZERS_SIZE> finalizers;
     };
 }
 
