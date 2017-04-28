73cc1fb30e2f1072228f64bf5165a543733d6b60
westelh <reo.retriever@gmail.com>
2017-04-28 17:44:22 +0900

    Brush up test source

--- src/test/singletonTest.cpp
+++ src/test/singletonTest.cpp
@@ -1,17 +1,21 @@
 #include <string>
 #include <gtest/gtest.h>
 #include "singleton/singleton.h"
+using namespace std::literals::string_literals;
 
 TEST(singleton, instance_reference) {
-    auto* ptr = &elh::singleton<int>::get_instance();    
+    auto* ptr = &elh::singleton<int>::get_instance();
+    // same
     auto* ptr2 = &elh::singleton<int>::get_instance();
+    // different
     auto* dptr = &elh::singleton<long>::get_instance();
+
     ASSERT_EQ(ptr, ptr2);
     ASSERT_NE(ptr, (int*)dptr);
 }
 
 TEST(singleton, constructing_instance) {
-    auto model = std::string("Weei");
+    auto model = "Weei"s;
     auto instance = elh::singleton<std::string>::get_instance("Weei");
     ASSERT_EQ(model, instance);
 }
